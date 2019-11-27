c3d5d6f3fd610390638ab15ff017747457be9683
issxjl2015 <issxjl2015@outlook.com>
2019-11-27 22:28:05 +0800

    新增了两个解答

---
+++ src/main/algorithms/algo_19_删除链表的倒数第N个节点/ListNode.java
@@ -0,0 +1,9 @@
+package main.algorithms.algo_19_删除链表的倒数第N个节点;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/algorithms/common/ListNode.java
@@ -0,0 +1,9 @@
+package main.algorithms.common;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
