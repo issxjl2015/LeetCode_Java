fc08e7afde279291bdb5f341ed614611182a64f5
issxjl2015 <issxjl2015@outlook.com>
2019-12-04 00:42:07 +0800

    用异或的方式能得到只出现一次的数字

---
+++ src/main/algorithms/algo_136_只出现一次的数字/Solution.java
@@ -0,0 +1,11 @@
+package main.algorithms.algo_136_只出现一次的数字;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+}
