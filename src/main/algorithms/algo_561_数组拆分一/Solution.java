9393e2d4c95aef901d085e0294ba85136b1f278b
issxjl2015 <issxjl2015@outlook.com>
2019-12-05 23:39:19 +0800

    数组拆分I

---
+++ src/main/algorithms/algo_561_数组拆分一/Solution.java
@@ -0,0 +1,14 @@
+package main.algorithms.algo_561_数组拆分一;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i = i + 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+}
