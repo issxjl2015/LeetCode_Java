69b08e4d9a8a96e2e859e9c361df11369a655b3b
issxjl2015 <issxjl2015@outlook.com>
2019-12-07 11:39:06 +0800

    计数质数方法一，首先维护一个质数标志数组，判断是否是质数，同时对于一些质数的倍数肯定也是非质数。最后统计质数标志为true的质数个数

---
+++ src/main/algorithms/algo_204_计数质数/Solution.java
@@ -0,0 +1,26 @@
+package main.algorithms.algo_204_计数质数;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int countPrimes(int n) {
+        boolean[] isPrim = new boolean[n];
+        // 将数组都初始化为true
+        Arrays.fill(isPrim, true);
+
+        for(int i = 2; i < n; i++) {
+            if (isPrim[i]) {
+                for (int j = 2 * i; j < n; j += i) {
+                    isPrim[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrim[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
