6179d9b37db83cba3f61fb83041479685c544474
issxjl2015 <issxjl2015@outlook.com>
2019-12-07 12:02:07 +0800

    遍历，并设置哑结点

---
+++ src/main/algorithms/algo_203_移除链表元素/ListNode.java
@@ -0,0 +1,9 @@
+package main.algorithms.algo_203_移除链表元素;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/algorithms/algo_203_移除链表元素/Solution.java
@@ -0,0 +1,25 @@
+package main.algorithms.algo_203_移除链表元素;
+
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null || head.next ==null) {
+            return null;
+        }
+        // 定义一个哑结点
+        ListNode dumb = new ListNode(0);
+        // 定义一个节点赋值为哑结点
+        dumb.next = head;
+        // 定义一个遍历节点
+        ListNode iter = dumb;
+        while (iter.next != null) {
+            ListNode curr = iter.next;
+            if (curr.val == val) {
+                iter.next = curr.next;
+                continue;
+            }
+            // 如果不相等，迭代节点向后移动一个
+            iter = iter.next;
+        }
+        return dumb.next;
+    }
+}
