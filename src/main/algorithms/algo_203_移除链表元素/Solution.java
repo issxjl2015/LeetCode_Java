016039755c9c94a366cdbbd59ef405b774184875
issxjl2015 <issxjl2015@outlook.com>
2019-12-07 12:03:14 +0800

    参数校验不需要

--- src/main/algorithms/algo_203_移除链表元素/Solution.java
+++ src/main/algorithms/algo_203_移除链表元素/Solution.java
@@ -2,9 +2,6 @@ package main.algorithms.algo_203_移除链表元素;
 
 public class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        if(head == null || head.next ==null) {
-            return null;
-        }
         // 定义一个哑结点
         ListNode dumb = new ListNode(0);
         // 定义一个节点赋值为哑结点
