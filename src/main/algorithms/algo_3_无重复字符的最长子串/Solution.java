93482e633c76156f98f946ba74c9091bbb65bb6a
issxjl2015 <issxjl2015@outlook.com>
2019-12-08 16:35:54 +0800

    用set来判断是否存在重复

---
+++ src/main/algorithms/algo_3_无重复字符的最长子串/Solution.java
@@ -0,0 +1,30 @@
+package main.algorithms.algo_3_无重复字符的最长子串;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() < 1) {
+            return -1;
+        }
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int len = solution.lengthOfLongestSubstring("abcabcbb");
+        System.out.println(len);
+    }
+}
