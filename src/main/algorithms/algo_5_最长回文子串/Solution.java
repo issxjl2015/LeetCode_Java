2881ed394336085ecdc24ab45f46c0f8dad00059
issxjl2015 <issxjl2015@outlook.com>
2019-12-08 15:46:07 +0800

    中心扩展法，需要考虑奇数和偶数长度的字符串

---
+++ src/main/algorithms/algo_5_最长回文子串/Solution.java
@@ -0,0 +1,35 @@
+package main.algorithms.algo_5_最长回文子串;
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expendAroundCenter(s, i, i);
+            int len2 = expendAroundCenter(s, i, i+1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private  int expendAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String ans = solution.longestPalindrome("ababb");
+        System.out.println(ans);
+    }
+}
