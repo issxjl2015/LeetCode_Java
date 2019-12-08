aaac7e545a5843dc508738346a90a5b4859d7cee
issxjl2015 <issxjl2015@outlook.com>
2019-12-08 18:28:28 +0800

    非递归，这个也很好理解。我感觉比递归的要好理解一些

---
+++ src/main/algorithms/algo_24_两两交换链表中的节点/SolutionTwo.java
@@ -0,0 +1,21 @@
+package main.algorithms.algo_24_两两交换链表中的节点;
+
+/**
+ *  非递归
+ * */
+public class SolutionTwo {
+    public ListNode swapPairs(ListNode head) {
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode temp = pre;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode start = temp.next;
+            ListNode end = temp.next.next;
+            temp.next = end;
+            start.next = end.next;
+            end.next = start;
+            temp = start;
+        }
+        return pre.next;
+    }
+}
