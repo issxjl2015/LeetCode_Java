4063c00459de1a5d5882625dd9bf841feb86daf3
issxjl2015 <issxjl2015@outlook.com>
2019-12-09 23:51:26 +0800

    寻找峰值

---
+++ src/main/algorithms/algo_162_寻找峰值/Solution.java
@@ -0,0 +1,11 @@
+package main.algorithms.algo_162_寻找峰值;
+
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1])
+                return i;
+        }
+        return nums.length - 1;
+    }
+}
