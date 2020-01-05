e17b2239f202e383b2e2f822bb4203c5817bebab
issxjl2015 <issxjl2015@outlook.com>
2020-01-05 11:54:50 +0800

    同样是递归处理

---
+++ src/main/algorithms/algo_101_对称二叉树/Solution.java
@@ -0,0 +1,20 @@
+package main.algorithms.algo_101_对称二叉树;
+
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+    
+    private boolean isSymmetric(TreeNode n1, TreeNode n2) {
+        if (n1 == null || n2 == null) {
+            return n1 == n2;
+        }
+        if (n1.val != n2.val) {
+            return false;
+        }
+        return isSymmetric(n1.left, n2.right) && isSymmetric(n1.right, n2.left);
+    }
+}
---
+++ src/main/algorithms/algo_101_对称二叉树/TreeNode.java
@@ -0,0 +1,10 @@
+package main.algorithms.algo_101_对称二叉树;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
