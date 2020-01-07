3c460dc5c7092479cd42eb079079d20248ae8d74
issxjl2015 <issxjl2015@outlook.com>
2020-01-07 22:41:22 +0800

    3的幂

---
+++ src/main/algorithms/algo_326_3的幂/Solution.java
@@ -0,0 +1,13 @@
+package main.algorithms.algo_326_3的幂;
+
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n < 1) {
+            return false;
+        }
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
