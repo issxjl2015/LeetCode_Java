cf8c06a600c2bdaae8f9fd9b91cea85009a3d426
issxjl2015 <issxjl2015@outlook.com>
2020-01-09 22:26:41 +0800

    最接近的K个点

---
+++ src/main/algorithms/algo_973_最接近原点的K个点/Solution.java
@@ -0,0 +1,22 @@
+package main.algorithms.algo_973_最接近原点的K个点;
+
+public class Solution {
+    public int[][] kClosest(int[][] points, int K) {
+        int[] dist = new int[points.length];
+        for (int i = 0; i < points.length; i++) {
+            dist[i] = distitan(points[i]);
+        }
+        Arrays.sort(dist);
+        int[][] ans = new int[K][2];
+        int j = 0;
+        for (int i = 0; i < points.length; i++) {
+            if (distitan(points[i]) <= dist[K-1]) {
+                ans[j++] = points[i];
+            }
+        }
+        return ans;
+    }
+    private int distitan(int[] point) {
+        return point[0] * point[0] + point[1] * point[1];
+    }
+}
