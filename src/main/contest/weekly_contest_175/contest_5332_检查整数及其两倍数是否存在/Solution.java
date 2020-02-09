0a34d10c7fa17b1f02bb473352c81b10d5a6269e
issxjl2015 <issxjl2015@outlook.com>
2020-02-09 21:07:03 +0800

    暴力法：因为数据量不大，可以使用暴力快速求解

---
+++ src/main/contest/weekly_contest_175/contest_5332_检查整数及其两倍数是否存在/Solution.java
@@ -0,0 +1,14 @@
+package main.contest.weekly_contest_175.contest_5332_检查整数及其两倍数是否存在;
+
+public class Solution {
+    public boolean checkIfExist(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j && (arr[i] * 2 == arr[j])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
