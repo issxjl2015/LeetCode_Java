515d9a013e53d7e33cfbddd0872ef2f16879e755
issxjl2015 <issxjl2015@outlook.com>
2020-02-09 22:00:03 +0800

    异位词就是字母个数一样，只是顺序不一样。

---
+++ src/main/contest/weekly_contest_175/contest_5333_制造字母异位词的最小步骤/Solution.java
@@ -0,0 +1,26 @@
+package main.contest.weekly_contest_175.contest_5333_制造字母异位词的最小步骤;
+
+public class Solution {
+    public int minSteps(String s, String t) {
+        int[] arr = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            arr[t.charAt(i) - 'a']--;
+        }
+        int sum = 0;
+        for (int c : arr) {
+            if (c > 0) {
+                sum += c;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int sum = solution.minSteps("aba", "bba");
+        System.out.println(sum);
+    }
+}
