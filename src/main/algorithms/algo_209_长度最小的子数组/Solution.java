2cf07fb23cda77c237531a8d95b4be50c43c625b
issxjl2015 <issxjl2015@outlook.com>
2020-05-10 21:24:29 +0800

    长度最长的子数组，采用双指针的方法

---
+++ src/main/algorithms/algo_209_长度最小的子数组/Solution.java
@@ -0,0 +1,20 @@
+package src.main.algorithms.algo_209_长度最小的子数组;
+
+public class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        int sum = 0;
+        int len = 0;
+        for (int j = 0; j < nums.length; j++) {
+            sum += nums[j];
+            while (sum >= s) {
+                len = len == 0 ? j - i + 1 : Math.min(len, j - i + 1);
+                sum -= nums[i++];
+            }
+        }
+        return len;
+    }
+}
