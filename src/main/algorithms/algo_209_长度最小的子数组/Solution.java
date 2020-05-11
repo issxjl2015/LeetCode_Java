e07869a439edb8db80466a1841cbda26cd6b1e06
issxjl2015 <issxjl2015@outlook.com>
2020-05-11 22:32:18 +0800

    新增一个暴力解法，把所有可能的子数组求和并更新ans，直到找到最优子数组且和满足sum >= s

--- src/main/algorithms/algo_209_长度最小的子数组/Solution.java
+++ src/main/algorithms/algo_209_长度最小的子数组/Solution.java
@@ -1,5 +1,11 @@
 package src.main.algorithms.algo_209_长度最小的子数组;
 
+/**
+ * 核心思想：滑动窗口
+ * 1、累加窗口内的值 sum
+ * 2、如果窗口内的值 大于 s， 那么计算len, 且 左窗口 右移，并更新sum的值
+ */
+
 public class Solution {
     public int minSubArrayLen(int s, int[] nums) {
         if (nums.length == 0) {
@@ -11,7 +17,9 @@ public class Solution {
         for (int j = 0; j < nums.length; j++) {
             sum += nums[j];
             while (sum >= s) {
+                // 获取最小的长度
                 len = len == 0 ? j - i + 1 : Math.min(len, j - i + 1);
+                // 左指针右移
                 sum -= nums[i++];
             }
         }
---
+++ src/main/algorithms/algo_209_长度最小的子数组/SolutionOne.java
@@ -0,0 +1,24 @@
+package src.main.algorithms.algo_209_长度最小的子数组;
+
+public class SolutionOne {
+    public int minSubArrayLen(int s, int[] nums) {
+        int ans = Integer.MAX_VALUE;
+        // 变量 i 从左到右遍历数组
+        for (int i  = 0; i < nums.length; i++) {
+            // 变量 j 从当前元素到数组尾部遍历
+            for (int j = i; j < nums.length; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += nums[k];
+                }
+                // 如果和sum 比 s 大
+                if (sum >= s) {
+                    // 更新 ans = min(ans, (j - i + 1))
+                    ans = Math.min(ans, j - i + 1);
+                    break;
+                }
+            }
+        }
+        return (ans != Integer.MAX_VALUE) ? ans : 0;
+    }
+}
