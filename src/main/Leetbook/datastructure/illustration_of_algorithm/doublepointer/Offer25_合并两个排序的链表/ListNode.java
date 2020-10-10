9a8b7502e0383a6cd937c1626706162a4288c0b4
issxjl2015 <issxjl2015@outlook.com>
2020-10-10 22:09:37 +0800

    双指针实现合并两个排序的链表

---
+++ src/main/Leetbook/datastructure/illustration_of_algorithm/doublepointer/Offer25_合并两个排序的链表/ListNode.java
@@ -0,0 +1,9 @@
+package Leetbook.datastructure.illustration_of_algorithm.doublepointer.Offer25_合并两个排序的链表;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/Leetbook/datastructure/illustration_of_algorithm/doublepointer/Offer25_合并两个排序的链表/Solution.java
@@ -0,0 +1,21 @@
+package Leetbook.datastructure.illustration_of_algorithm.doublepointer.Offer25_合并两个排序的链表;
+
+public class Solution {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
+}
