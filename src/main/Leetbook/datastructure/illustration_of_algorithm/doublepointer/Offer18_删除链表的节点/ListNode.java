c617d47a3da9cb9cb597e7ec72bab7386aeba77c
issxjl2015 <issxjl2015@outlook.com>
2020-10-10 21:25:31 +0800

    双指针实现删除链表节点

---
+++ src/main/Leetbook/datastructure/illustration_of_algorithm/doublepointer/Offer18_删除链表的节点/ListNode.java
@@ -0,0 +1,9 @@
+package Leetbook.datastructure.illustration_of_algorithm.doublepointer.Offer18_删除链表的节点;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
