e5b110d56712dedb340f4f8cef5992a6725037a4
issxjl2015 <issxjl2015@outlook.com>
2020-11-09 23:06:18 +0800

    二分查找寻找峰值

---
+++ src/main/algorithms/algo_162_寻找峰值/Solution2.java
@@ -0,0 +1,16 @@
+package algorithms.algo_162_寻找峰值;
+
+public class Solution2 {
+    public int findPeakElement(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}
