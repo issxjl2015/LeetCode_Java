7aa26dca30623067aa8b95eb9d471983b9856b18
issxjl2015 <issxjl2015@outlook.com>
2020-11-10 23:07:51 +0800

    求众数

---
+++ src/main/algorithms/algo_229_求众数II/Solution.java
@@ -0,0 +1,22 @@
+package algorithms.algo_229_求众数II;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        for (int i : map.keySet()) {
+            if (map.get(i) * 3 > nums.length) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
